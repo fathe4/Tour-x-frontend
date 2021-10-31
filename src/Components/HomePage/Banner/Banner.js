408c94baebe553deea5dbc2ce262bb431d6d19fd
unknown <freelancerfathe@gmail.com>
2021-10-31 23:51:57 +0600

    slider added

--- src/Components/HomePage/Banner/Banner.js
+++ src/Components/HomePage/Banner/Banner.js
@@ -1,25 +1,15 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import './Banner'
 
 const Banner = () => {
     return (
         <div>
             <Carousel fade>
+
+
                 <Carousel.Item>
-                    <img
-                        className="d-block w-100"
-                        src="https://demo.egenslab.com/html/tourx/assets/images/banners/banner-3.png"
-                        alt="First slide"
-                    />
-                    <Carousel.Caption className=''>
-
-                        <h1>Amazing Tour In Paris Hill Tour</h1>
-                        <h3 className='fw-bold'>6 Days/ 7 Nights</h3>
-                        <button className='btn primary-bg-color'>Book Now</button>
-
-                    </Carousel.Caption>
-                </Carousel.Item>
-                {/* <Carousel.Item>
                     <img
                         className="d-block w-100"
                         src="https://demo.egenslab.com/html/tourx/assets/images/banners/banner-1.png"
@@ -27,10 +17,24 @@ const Banner = () => {
                     />
 
                     <Carousel.Caption>
-                        <h3>Second slide label</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <h1 className='fw-bold'>Get Your Tour From us</h1>
+                        <p className='fw-bold'>Tour X is the best platform for tourism</p>
+                        <Link to='/tour-packages'><button className='btn primary-bg-color'>Check Our Tour packages</button></Link>
                     </Carousel.Caption>
-                </Carousel.Item> */}
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        className="d-block w-100"
+                        src="https://demo.egenslab.com/html/tourx/assets/images/banners/banner-3.png"
+                        alt="Second slide"
+                    />
+
+                    <Carousel.Caption>
+                        <h1 className='fw-bold'>We are the trusted tourism agents</h1>
+                        <p className='fw-bold'>Get your best tour ever from us</p>
+                        <Link to='/tour-packages'><button className='btn primary-bg-color'>Check Our Tour packages</button></Link>
+                    </Carousel.Caption>
+                </Carousel.Item>
 
             </Carousel>
         </div>
